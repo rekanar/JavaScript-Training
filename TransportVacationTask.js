0211fc13f67b7f79b5ad322e71b1dd50b4e4ed6a
rekanar <santhoshrekanar@gmail.com>
2017-04-24 12:28:07 +0530

    Create TransportVacationTask.js

---
+++ TransportVacationTask.js
@@ -0,0 +1,14 @@
+function rentalCarCost(d) 
+{   
+  if (d < 3) 
+  {    
+   return d * 40;  
+   } 
+   else if (d > 2 && d < 7) 
+   {    
+    return (d * 40) - 20;  
+    } 
+  else if (d > 6) 
+    {    return (d * 40) - 50;  
+    }
+}
