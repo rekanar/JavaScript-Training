eaed15153f842f28b6b2179cdf0438b81695d942
rekanar <santhoshrekanar@gmail.com>
2017-04-24 13:01:44 +0530

    Update FindingMiddleCharacter.js

--- FindingMiddleCharacter.js
+++ FindingMiddleCharacter.js
@@ -3,21 +3,18 @@ function getMiddle(s)
   var withoutSpace = s.replace(/ /g, "");  
   var len = withoutSpace.length;  
   var isEven = evenOrOdd(len);
-  var middle = len / 2; 
+  var middle = len / 2;  
   if (isEven) 
-  {    
-    return s.substring(middle - 1, middle + 1);  
-    } 
-  else   
   {   
-    return s.charAt(Math.round(middle - 1));  }
-    }
-    
+    return s.substring(middle - 1, middle + 1); 
+  } 
+  else   {    
+    return s.charAt(Math.round(middle - 1));  
+  }}
 function evenOrOdd(len) 
-{  
-  if (len % 2 == 0)    
-  return true;  
+{  if (len % 2 == 0)    
+    return true; 
   else    
-  return false;
-  
-  };
+    return false;
+};
+
