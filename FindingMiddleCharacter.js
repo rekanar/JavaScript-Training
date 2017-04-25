9cc6216f20864dc181b26566ccf1269378427fc0
rekanar <santhoshrekanar@gmail.com>
2017-04-25 18:32:10 +0530

    Update FindingMiddleCharacter.js

--- FindingMiddleCharacter.js
+++ FindingMiddleCharacter.js
@@ -1,1 +1,16 @@
-function getMiddle(s){  var withoutSpace = s.replace(/ /g, "");  var len = withoutSpace.length;  var middle = len / 2;  var middleChar;  if (IsEven(len)) {    middleChar = s.substring(middle - 1, middle + 1);  } else   {   middleChar = s.charAt(Math.round(middle - 1));  }  return middleChar;}
+function getMiddle(s)
+{  
+  var withoutSpace = s.replace(/ /g, "");  
+  var len = withoutSpace.length;  
+  var middle = len / 2;  
+  var middleChar;  
+  if (IsEven(len)) 
+  {    
+    middleChar = s.substring(middle - 1, middle + 1);  
+  } 
+  else
+  {   
+    middleChar = s.charAt(Math.round(middle - 1));  
+  }  
+  return middleChar;
+}
